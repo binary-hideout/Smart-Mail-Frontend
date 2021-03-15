7ff57cf6dd9e1bad615c9358e09220a557d852dd
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2021-03-15 05:13:26 -0600

    Add Dashboard component

---
+++ app/src/components/Dashboard.vue
@@ -0,0 +1,32 @@
+<template>
+<v-app id="inspire">
+    <ApplicationBar />
+    <v-main class="grey lighten-3">
+        <v-container>
+            <v-row>
+                <v-col cols="2">
+                    <Sidebar />
+                </v-col>
+
+                <v-col>
+                    <Contacts />
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-main>
+</v-app>
+</template>
+
+<script>
+import Sidebar from './Sidebar';
+import ApplicationBar from './ApplicationBar';
+import Contacts from './Contacts';
+
+export default {
+    components: {
+        ApplicationBar,
+        Sidebar,
+        Contacts,
+    }
+}
+</script>
