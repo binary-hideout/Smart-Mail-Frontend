0ac4c7065032d7c73f2325f7390264f5445e6464
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2021-03-15 05:12:17 -0600

    Add Actions component

---
+++ app/src/components/Actions.vue
@@ -0,0 +1,8 @@
+<template v-slot:item.actions="{ item }">
+<v-icon small class="mr-2" @click="editItem(item)">
+    mdi-pencil
+</v-icon>
+<v-icon small @click="deleteItem(item)">
+    mdi-delete
+</v-icon>
+</template>
