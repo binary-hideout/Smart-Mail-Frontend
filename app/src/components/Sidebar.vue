3d81c295b3d7b9a1c9af9990932bfc4f88a7215d
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2021-03-15 05:13:36 -0600

    Add Sidebar component

---
+++ app/src/components/Sidebar.vue
@@ -0,0 +1,37 @@
+<template>
+<v-sheet rounded="lg">
+    <v-list color="transparent">
+        <v-list-item v-for="tag in tags" :key="tag" link>
+            <v-list-item-content>
+                <v-list-item-title>
+                    {{ tag }}
+                </v-list-item-title>
+            </v-list-item-content>
+        </v-list-item>
+
+        <v-divider class="my-2"></v-divider>
+
+        <v-list-item link color="grey lighten-4">
+            <v-list-item-content>
+                <v-list-item-title>
+                    Refresh
+                </v-list-item-title>
+            </v-list-item-content>
+        </v-list-item>
+    </v-list>
+</v-sheet>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            tags: [
+                'To be completed',
+                'In progress',
+                'Completed',
+            ],
+        }
+    }
+}
+</script>
